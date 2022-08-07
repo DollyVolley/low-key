d9a9ab75b0efd1cbdbee5cc21d821ce3e2c1f0d2
DollyVolley <oliver.schwarz@block42.tech>
2022-08-07 15:50:31 +0200

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -58,10 +58,11 @@ module.exports = {
       }]
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'public', 'index.html')
-      }),
-
+      new CopyWebpackPlugin({
+        patterns: [
+            { from: 'public' }
+        ]
+    }),
       new webpack.ProvidePlugin({
           Buffer: ['buffer', 'Buffer'],
       }),
