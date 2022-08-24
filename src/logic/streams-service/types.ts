ed9ee79d40a0d55276e31501f9ecc7c6edeb0a9e
DollyVolley <oliver.schwarz@block42.tech>
2022-08-24 23:33:07 +0200

    add first queue implementation

--- src/logic/streams-service/types.ts
+++ src/logic/streams-service/types.ts
@@ -1,6 +1,27 @@
 import { ChatMessage } from '@/types/chat'
 import streamsLib from './lib/streams'
 
+export type ActionQueue = {[key in string]: StreamsQueueRequest[]}
+
+export type StreamsAction = (client: ActiveClient, id: string) => Promise<StreamsResponse>
+
+export interface StreamsQueueRequest {
+    id: string,
+    action: StreamsAction
+}
+
+export interface StreamsResponse {
+    id: string,
+    client: ActiveClient,
+    exportedClient?: ArchiveClient,
+    messages?: ChatMessage[]
+}
+
+export interface MessageResponse {
+    client: ActiveClient,
+    messages: ChatMessage[],
+}
+
 export type StreamsActor = streamsLib.Author | streamsLib.Subscriber
 
 export interface MessageServiceConfig {
@@ -8,7 +29,7 @@ export interface MessageServiceConfig {
 }
 
 export interface ActiveClient extends BaseClient {
-    streamsClient: StreamsActor,
+    streamsClient: StreamsActor | null,
 }
 
 export interface ArchiveClient extends BaseClient {
@@ -33,7 +54,3 @@ export enum ClientType {
     SUBSCRIBER = "SUBSCRIBER"
 }
 
-export interface MessageResponse {
-    client: ActiveClient,
-    messages: ChatMessage[],
-}
